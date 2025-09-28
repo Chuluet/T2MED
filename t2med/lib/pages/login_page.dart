806727eacef68774c1d759e514c65f6323e3b238
Chuluet <santiagotoromedina@gmail.com>
2025-09-28 14:37:14 -0500

    Correccion de mensajes

--- t2med/lib/pages/login_page.dart
+++ t2med/lib/pages/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:t2med/pages/home_page.dart';
 import 'package:t2med/services/user_service.dart';
 import 'package:t2med/widgets/input_decorations.dart';
 
